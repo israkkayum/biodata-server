106f59d00b2d971a6d532c4081e11d47cc828a32
israkkayum <israk.kayum@gmail.com>
2022-08-21 02:03:44 +0600

    make admin remove

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ async function run() {
 
     app.put("/users/admin", async (req, res) => {
       const user = req.body;
-      const filter = { email: user.e };
+      const filter = { email: user.id };
       const updateDoc = { $set: { role: "admin" } };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.json(result);
