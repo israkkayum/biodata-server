fb42e92f112ed166ce44f945b887c76e6be5feb0
israkkayum <israk.kayum@gmail.com>
2022-08-25 00:38:27 +0600

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 65000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uf0qwiy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipwmulq.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
